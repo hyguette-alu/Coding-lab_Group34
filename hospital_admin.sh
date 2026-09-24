e6cba8dfba39182db5e72746a3e2886980d44e46
hyguette-alu <i.hyguette@alustudent.com>
2026-09-24 18:26:47 +0200

    (admin) add: hospital_admin.sh initialize_system function

---
+++ hospital_admin.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ACTIVE_DIR="active_logs"
+ARCHIVE_DIR="archived_logs"
+REPORT_DIR="reports"
+
+# ----The Architect ----
+initialize_system() {
+    echo "Initializing KNH system directories..."
+    for dir in "$ACTIVE_DIR" "$ARCHIVE_DIR" "$REPORT_DIR"; do
+        if [ -d "$dir" ]; then
+            echo "$dir already exists, skipping."
+        else
+            echo "Creating $dir directory..."
+            mkdir -p "$dir"
+        fi
+    done
+}
+
+secure_data() {
+
+}
